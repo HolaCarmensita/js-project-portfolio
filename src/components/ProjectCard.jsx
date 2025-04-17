c01b3a1595b08d6d5ad2627c9b475d50e451077c
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2025-04-17 12:21:37 +0200

    nu använder Techniques, ProjectCard och projects projects.json istället för hårdkodat/inget innehåll

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,30 @@
-function ProjectCard({ title, description, techniques }) {
+import Techniques from './Techniques';
+import Button from './Button';
+
+function ProjectCard({ project }) {
+  console.log(`path`, project.image);
   return (
     <article>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      {/*techniques-component + knappar till netlify och github */}
+      <img
+        src={project.image}
+        alt={`${project.name} preview`}
+        style={{ width: '450px', height: '300px', objectFit: 'cover' }}
+      />
+      <h3>{project.name}</h3>
+      <p>{project.description}</p>
+      <Button
+        text='Live demo'
+        href={project.netlify}
+        target='_blank'
+        rel='noopener noreferrer'
+      />
+      <Button
+        text='GitHub'
+        href={project.github}
+        target='_blank'
+        rel='noopener noreferrer'
+      />
+      <Techniques list={project.tags} />
     </article>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,16 @@
+import projectsData from '../data/projects.json';
+import ProjectCard from './ProjectCard';
+
 function Projects() {
   return (
     <section>
       <h2>Featured Projects</h2>
       <p>This is the Projects section</p>
-
-      {/* ProjectCard-komponenter här */}
+      <div className='project-grid'>
+        {projectsData.projects.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
     </section>
   );
 }
