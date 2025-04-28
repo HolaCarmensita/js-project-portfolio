50fa7a0e67cabd60c88a0a373ff72bd12e7751dc
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2025-04-28 15:42:19 +0200

    min/max-with fix in the LayoutWrapper.jsx fixed

--- src/components/LayoutWrapper.jsx
+++ src/components/LayoutWrapper.jsx
@@ -1,23 +1,26 @@
 import styled from 'styled-components';
 
 const LayoutWrapper = styled.div`
-  max-width: 100%; /* Fulll bredd på mobila enheter */
+  max-width: 100%; /* Full bredd på mobila enheter */
   padding-inline: 2rem;
   padding-block: 2rem;
 
-  @media (min-width: 768px) and (max-width: 1024px) {
+  @media (min-width: 768px) and (max-width: 820px) {
     max-width: 550px; /* Sätter max-bredd för mindre tablet-enheter (iPad) */
     padding-inline: 3rem;
     padding-block: 3rem;
     margin: 0 auto;
   }
 
-  @media (min-width: 820px) and (max-width: 1024px) {
-    max-width: 590px; /* Sätter max-bredd för större tablet-enheter (iPad) */
+  @media (min-width: 821px) and (max-width: 1024px) {
+    max-width: 750px; /* Sätter max-bredd för större tablet-enheter (iPad) */
+    padding-inline: 3rem;
+    padding-block: 3rem;
+    margin: 0 auto;
   }
 
   @media (min-width: 1025px) {
-    max-width: 1000px; /* Större max-bredd för desktop */
+    max-width: 1200px; /* Större max-bredd för desktop (ändrat från 600px) */
     padding-inline: 4rem;
     padding-block: 4rem;
     margin: 0 auto;
