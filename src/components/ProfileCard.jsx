1bf41ba9c4b7fa02606f34b2cf4a9f0fd9b8c34d
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2025-05-01 13:04:09 +0200

    changed my mind, I want to use svg code instead of img

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -41,7 +41,7 @@ const FullWidthSection = styled.section`
   position: relative;
 `;
 
-const Circle = styled.img`
+const SvgWrapper = styled.div`
   width: 15vw;
   max-width: 250px;
   position: absolute;
@@ -60,7 +60,23 @@ function ProfileCard() {
         </ProfileCardContainer>
       </LayoutWrapper>
       <SocialMedia variant='about' />
-      <Circle src='/shapes/circle.svg' alt='' aria-hidden='true'></Circle>
+
+      <SvgWrapper>
+        <svg
+          width='245'
+          height='245'
+          viewBox='0 0 245 245'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            fillRule='evenodd'
+            clipRule='evenodd'
+            d='M122.5 245C190.155 245 245 190.155 245 122.5C245 54.8451 190.155 0 122.5 0C54.8451 0 0 54.8451 0 122.5C0 190.155 54.8451 245 122.5 245ZM122.5 196C163.093 196 196 163.093 196 122.5C196 81.9071 163.093 49 122.5 49C81.9071 49 49 81.9071 49 122.5C49 163.093 81.9071 196 122.5 196Z'
+            fill='#34EDB3'
+          />
+        </svg>
+      </SvgWrapper>
     </FullWidthSection>
   );
 }
